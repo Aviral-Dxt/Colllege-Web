fc7c64740a92af637a75a6d79549ac87301d2611
Aviral Dixit <aviraldxt345@gmail.com>
2025-01-03 19:05:33 +0530

    ContactCmpltd

--- Edusity/src/components/contact/Contact.jsx
+++ Edusity/src/components/contact/Contact.jsx
@@ -4,9 +4,37 @@ import msg_icon from "../../assets/msg-icon.png"
 import mail_icon from "../../assets/mail-icon.png"
 import phone_icon from "../../assets/phone-icon.png"
 import location_icon from "../../assets/location-icon.png"
-
+import arrow_2 from '../../assets/white-arrow.png'
 
 const Contact = () => {
+
+
+    const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "9a6966d4-7d40-4308-bab4-f1785a7d6ad1");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+
   return (
     <div className='contact'>
     <div className="contact-col">
@@ -23,9 +51,16 @@ const Contact = () => {
 
     <div className="contact-col">
           
-          <form>
-            
+          <form onSubmit={onSubmit}>
+             <label>Enter Your Name</label>
+             <input type="text" name='name' placeholder='Enter your name' required />
+             <label>Phone Number</label>
+             <input type="tel" name="phone" placeholder='Enter your Phone number' required />
+             <label>Write Your Message </label>
+             <textarea name="message" rows="6" placeholder='Write your message here' required></textarea>
+             <button type='submit' className='btn drk-btn'>Submit Now <img src={arrow_2} alt="w-arr" /></button>
           </form>
+          <span>{result}</span>
 
     </div>
     </div>
